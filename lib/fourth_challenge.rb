e3def98196f8356ea700274279a445d4c3edcf4b
Yuki N <lunarbase@outlook.com>
2018-11-05 01:52:07 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,7 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
         {:name => "Steven", :age => "30", :attitude => "confused"},
-        {}
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
